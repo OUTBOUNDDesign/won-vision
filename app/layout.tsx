afb46ca520090c1758d41fef68ee55e6ad7a24ef
Kiran Chong <admin@outbounddesign.com.au>
2026-05-07 16:50:52 +1000

    fix(home): add id="top" to body for #top anchor links

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>
+      <body id="top">
         {children}
         <Script src="/script.js" strategy="afterInteractive" />
       </body>
